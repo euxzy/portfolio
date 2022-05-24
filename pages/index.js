6753e5e6a11ad96063b8f5d458538278a0aada72
euxzy <rijal.m12l@gmail.com>
2022-05-24 15:00:51 +0700

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex w-full h-screen items-center justify-center text-5xl">
-      <div>Home</div>
+    <div className="flex h-screen w-full items-center justify-evenly">
+      <div className="w-1/3 text-right text-8xl">Home</div>
+      <div className="w-1/3">
+        <div className="-mb-5 text-base">Hi! My name is</div>
+        <div className="-ml-1 text-7xl">euxzy</div>
+      </div>
     </div>
   );
 }
