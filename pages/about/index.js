17b30687740b9fae317010e8f7cab976ed733782
euxzy <rijal.m12l@gmail.com>
2022-05-24 15:10:10 +0700

    add about page

---
+++ pages/about/index.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="text-9xl">About</div>
+    </div>
+  );
+}
