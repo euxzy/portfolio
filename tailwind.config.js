5fc05e70831d737aba86e4c9a0374fc9aee751d5
euxzy <rijal.m12l@gmail.com>
2022-05-24 14:35:17 +0700

    update home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: '#1b1b1b',
+          light: '#f1f1f1',
+        },
+        secondary: {
+          dark: '#0d0d0d',
+          light: '#999999',
+        },
+      },
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
