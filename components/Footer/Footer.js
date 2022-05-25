cc2e7cef4187d6e297fe8bef4fbe771e165f8a0b
euxzy <rijal.m12l@gmail.com>
2022-05-25 17:47:08 +0700

    update footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="fixed right-0 bottom-0 z-40 flex w-full justify-end pb-28">
       <div className="flex w-1/2 items-baseline justify-end">
         <div className="flex w-2/3 flex-col items-end">
-          <div className="h-1 w-1/4 bg-primary-light" />
+          <div className="h-1 w-2/5 bg-primary-light" />
           <div className="mt-1 h-1 w-full bg-primary-light" />
           <div className="mt-1 h-1 w-2/3 bg-primary-light" />
         </div>
