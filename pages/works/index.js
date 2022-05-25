596f3c77de5a6ab0c63965d11ba5320903eb041f
euxzy <rijal.m12l@gmail.com>
2022-05-25 17:28:10 +0700

    add works page

---
+++ pages/works/index.js
@@ -0,0 +1,16 @@
+import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Navbar/Navbar';
+
+export default function Works() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="text-8xl">Works</div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
