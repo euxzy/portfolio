e0eabe5cd44327f46e1fc57fb5587d584edca3ee
euxzy <rijal.m12l@gmail.com>
2022-05-25 19:43:43 +0700

    add custom cursor

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [customCursor, setCustomCursor] = useState('default');
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener('mousemove', mouseMove);
+  }, []);
+
+  const variants = {
+    default: { x: mousePosition.x - 32, y: mousePosition.y - 32 },
+    inverse: { x: mousePosition.x - 32, y: mousePosition.y - 32, mixBlendMode: 'difference' },
+  };
+
+  const cursorIn = () => setCustomCursor('inverse');
+  const cursorOut = () => setCustomCursor('default');
+
+  return (
+    <>
+      <div onMouseEnter={cursorIn} onMouseLeave={cursorOut}>
+        <Component {...pageProps} />
+      </div>
+
+      <motion.div className="pointer-events-none fixed left-0 top-0 z-50 h-16 w-16 rounded-full bg-primary-light" variants={variants} animate={customCursor} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
