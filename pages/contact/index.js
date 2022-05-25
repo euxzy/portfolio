0bc53e63a6099b6fcb145b1d4042ec1e1a8b84db
euxzy <rijal.m12l@gmail.com>
2022-05-25 17:12:52 +0700

    update contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,8 +2,44 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <div>Contact</div>
+    <div className="flex h-screen w-full items-center justify-evenly">
+      <div className="w-1/3 text-right">
+        <div>
+          <h4 className="text-sm font-extralight">Contact</h4>
+          <h3 className="text-lg font-normal">+62 858 xxxx xxxx</h3>
+        </div>
+      </div>
+      <div className="w-1/3">
+        <form>
+          <div className="w-4/5">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="mb-3 w-full rounded-md border border-secondary-light bg-primary-dark px-4 py-1 placeholder:text-secondary-light focus:border-primary-light focus:outline-none"
+              placeholder="Name"
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="mb-3 w-full rounded-md border border-secondary-light bg-primary-dark px-4 py-1 placeholder:text-secondary-light focus:border-primary-light focus:outline-none"
+              placeholder="Email"
+            />
+            <textarea
+              name="email"
+              id="email"
+              className="mb-2 h-40 w-full rounded-md border border-secondary-light bg-primary-dark px-4 py-1 placeholder:text-secondary-light focus:border-primary-light focus:outline-none"
+              placeholder="Write your message..."
+            />
+            <input
+              type="submit"
+              value="Send Message"
+              className="w-full rounded-md border border-secondary-light bg-primary-dark px-4 py-1 text-secondary-light transition duration-300 ease-in-out placeholder:text-secondary-light hover:border-primary-light hover:bg-secondary-dark hover:text-primary-light hover:outline-none focus:border-primary-light focus:outline-none"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
