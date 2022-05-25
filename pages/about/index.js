bea6e21242e25ad2bb097a7b4ae3acdb30db2f78
euxzy <rijal.m12l@gmail.com>
2022-05-25 17:06:38 +0700

    update about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,19 @@
+import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Navbar/Navbar';
+
 export default function About() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <div className="text-9xl">About</div>
-    </div>
+    <>
+      <Navbar />
+
+      <div className="flex h-screen w-full items-center justify-evenly">
+        <div className="w-1/3 text-right text-9xl">About</div>
+        <div className="w-1/3 text-justify">
+          <div className="text-lg">Hello! My name is Muhamad Rijal, you can call me Rijal, Euxzy, CicakUngu or whatever you want.</div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
   );
 }
