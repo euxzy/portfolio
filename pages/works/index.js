c5c6092a81a0a7dcfa045b29b48dc1d3fad8e547
euxzy <rijal.m12l@gmail.com>
2022-05-31 14:47:03 +0700

    update works page

--- pages/works/index.js
+++ pages/works/index.js
@@ -1,11 +1,23 @@
-import Layouts from '../../components/Layouts/Layouts';
+import Layouts from '../../components/Layouts';
 
 export default function Works() {
   return (
     <>
-      <Layouts pageTitle="| Works">
-        <div className="flex h-screen w-full items-center justify-center">
-          <div className="text-8xl">Works</div>
+      <Layouts pageTitle=" | Works">
+        <div className="relative flex h-screen w-full items-center justify-center overflow-hidden">
+          <div className="absolute bottom-0 left-0 text-9xl font-bold text-secondary-dark">WORKS</div>
+
+          <div className="relative flex w-4/5 items-center justify-evenly">
+            <div className="h-44 w-1/4 rounded-md bg-primary-light">
+              <div>Project 1</div>
+            </div>
+            <div className="h-44 w-1/4 rounded-md bg-primary-light">
+              <div>Project 1</div>
+            </div>
+            <div className="h-44 w-1/4 rounded-md bg-primary-light">
+              <div>Project 1</div>
+            </div>
+          </div>
         </div>
       </Layouts>
     </>
