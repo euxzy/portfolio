2e9b77f4eb1910b78e0aecf22a68c4deef0498f3
Ηͥ͢Ꭿͣ͜ℕͫ͜Ѕ͢×፝֟͜× <77654205+Hansalrl@users.noreply.github.com>
2022-06-26 16:40:38 +0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
     <Layouts>
       <section className="relative flex w-full flex-col items-center justify-evenly py-40 md:h-screen md:flex-row md:overflow-hidden">
         <motion.span {...FadeAnimation} className="title-page">
-          EUXZY
+          HANSALRL
         </motion.span>
 
         <motion.div {...ContentAnimation} className="relative order-2 flex w-1/2 justify-end md:order-1 md:w-1/3">
@@ -23,7 +23,7 @@ const Home = () => {
 
         <motion.div {...ContentAnimation} className="relative order-1 w-1/3 md:order-2">
           <h3 className="-mb-5 text-base font-normal">Hi! My name is</h3>
-          <h1 className="-ml-1 mb-1 text-7xl font-bold text-primary-light">euxzy</h1>
+          <h1 className="-ml-1 mb-1 text-7xl font-bold text-primary-light">hans</h1>
           <h4 className="text-sm font-light transition-all duration-500 hover:text-primary-light">Front-end Dev | Network Engineer</h4>
         </motion.div>
       </section>
